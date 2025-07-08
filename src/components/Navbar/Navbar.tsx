caf54383d11a4b4035f39718d57b4e9e6b6749e1
Steddy <138579152+Steddyz@users.noreply.github.com>
2025-07-08 11:53:19 +0300

    first commit and add Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import styles from "./Navbar.module.css";
+import "../../index.css";
+import Logo from "../../assets/Logo.png";
+
+const Navbar = () => {
+  return (
+    <header className="container">
+      <div className={styles.navbarContainer}>
+        <div className={styles.logo}>
+          <a href="#">
+            <img src={Logo} alt="logo" />
+          </a>
+        </div>
+        <nav className={styles.navLinks}>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Eposides</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
